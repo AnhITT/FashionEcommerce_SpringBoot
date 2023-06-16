bbb689b0c1a91baa32115f94bd3b1b5b16ce0aa8
Lam Anh <lamanhkk@gmail.com>
2023-06-16 20:08:28 +0700

    update

--- src/main/java/DANHHT/Fashion/controller/CheckOutController.java
+++ src/main/java/DANHHT/Fashion/controller/CheckOutController.java
@@ -1,5 +1,6 @@
 package DANHHT.Fashion.controller;
 
+
 import DANHHT.Fashion.dto.CartDto;
 import DANHHT.Fashion.model.Invoice;
 import DANHHT.Fashion.model.User;
@@ -7,6 +8,10 @@ import DANHHT.Fashion.service.CartService;
 import DANHHT.Fashion.service.InvoiceService;
 import DANHHT.Fashion.service.ProductService;
 import DANHHT.Fashion.service.UserService;
+import DANHHT.Fashion.service.impl.PaypalService;
+import com.paypal.api.payments.Links;
+import com.paypal.api.payments.Payment;
+import com.paypal.base.rest.PayPalRESTException;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -26,6 +31,11 @@ public class CheckOutController {
     private CartService cartService;
     @Autowired
     private UserService userService;
+    @Autowired
+    PaypalService service;
+
+    public static final String SUCCESS_URL = "checkout/checkoutSuccess";
+    public static final String CANCEL_URL = "checkout/checkoutFaild";
     @GetMapping("")
     public String checkOut(Model model){
         model.addAttribute("all_items_in_shoppingcart", cartService.GetAllItem());
@@ -54,4 +64,46 @@ public class CheckOutController {
         invoiceService.Remove(id);
         return "redirect:/manageOrder";
     }
+
+    @PostMapping("/pay")
+    public String payment(@ModelAttribute("invoice") Invoice invoice) {
+        try {
+            double totalAmouth = cartService.GetAmount();
+            Payment payment = service.createPayment(totalAmouth,
+                   "USD", "PAYPAL" ,
+                    "NONE", "",
+                    "http://localhost:8080/" + CANCEL_URL,
+                    "http://localhost:8080/" + SUCCESS_URL);
+            for(Links link:payment.getLinks()) {
+                if(link.getRel().equals("approval_url")) {
+                    return "redirect:"+link.getHref();
+                }
+            }
+
+        } catch (PayPalRESTException e) {
+
+            e.printStackTrace();
+        }
+        return "redirect:/";
+    }
+
+    @GetMapping(value = CANCEL_URL)
+    public String cancelPay() {
+        return "checkout/checkoutFaild";
+    }
+
+    @GetMapping(value = SUCCESS_URL)
+    public String successPay(@RequestParam("paymentId") String paymentId, @RequestParam("PayerID") String payerId) {
+        try {
+            Payment payment = service.executePayment(paymentId, payerId);
+            System.out.println(payment.toJSON());
+            if (payment.getState().equals("approved")) {
+                return "checkout/checkoutSuccess";
+            }
+        } catch (PayPalRESTException e) {
+            System.out.println(e.getMessage());
+        }
+        return "redirect:/";
+    }
+
 }
